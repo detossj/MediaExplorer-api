c8956fb467765e92aad3b32440a503f4cebae156
detossj <jorgerubilarssj@gmail.com>
2025-06-09 17:31:07 -0400

    functions added

--- app/src/main/java/com/deto/mediaexplorer/data/remote/services/ElementService.kt
+++ app/src/main/java/com/deto/mediaexplorer/data/remote/services/ElementService.kt
@@ -1,4 +1,16 @@
 package com.deto.mediaexplorer.data.remote.services
 
+
+import com.deto.mediaexplorer.data.Element
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ElementService {
+
+    @GET("api/elements")
+    suspend fun getElements(): List<Element>
+
+    @POST("api/elements")
+    suspend fun addElement(@Body category: Element): Element
 }
