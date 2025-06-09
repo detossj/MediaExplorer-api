f0edd3570157aa5577dc0f7ecddd3c61ab3fc436
detossj <jorgerubilarssj@gmail.com>
2025-06-09 17:27:54 -0400

    functions added

--- app/src/main/java/com/deto/mediaexplorer/data/remote/services/AuthService.kt
+++ app/src/main/java/com/deto/mediaexplorer/data/remote/services/AuthService.kt
@@ -1,4 +1,19 @@
 package com.deto.mediaexplorer.data.remote.services
 
+import com.deto.mediaexplorer.model.LoginRequest
+import com.deto.mediaexplorer.model.LoginResponse
+import com.deto.mediaexplorer.model.User
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface AuthService {
+    @POST("api/login")
+    suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @GET("api/profile")
+    suspend fun getUser(): User
+
+    @POST("logout")
+    suspend fun logout()
 }
