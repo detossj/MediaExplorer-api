770a869c12d5834ba4bf41b3f9b6ff8b65acacea
detossj <jorgerubilarssj@gmail.com>
2025-06-09 17:52:53 -0400

    apiServices added

--- app/src/main/java/com/deto/mediaexplorer/data/AppContainer.kt
+++ app/src/main/java/com/deto/mediaexplorer/data/AppContainer.kt
@@ -1,10 +1,17 @@
 package com.deto.mediaexplorer.data
 
 import android.content.Context
+import com.deto.mediaexplorer.data.remote.ApiClient
+import com.deto.mediaexplorer.data.remote.services.AuthService
+import com.deto.mediaexplorer.data.remote.services.CategoryService
+import com.deto.mediaexplorer.data.remote.services.ElementService
 
 interface AppContainer {
     val categoryRepository : CategoryRepository
     val elementRepository : ElementRepository
+    val authApiService: AuthService
+    val categoryApiService: CategoryService
+    val elementApiService: ElementService
 }
 
 class AppDataContainer(private val context: Context) : AppContainer {
@@ -15,6 +22,18 @@ class AppDataContainer(private val context: Context) : AppContainer {
 
     override val elementRepository: ElementRepository by lazy {
         LocalElementRepository(AppDatabase.getDatabase(context).elementDao())
+    }
+
+    override val authApiService: AuthService by lazy {
+        ApiClient.create(context).create(AuthService::class.java)
+    }
+
+    override val categoryApiService: CategoryService by lazy {
+        ApiClient.create(context).create(CategoryService::class.java)
+    }
+
+    override val elementApiService: ElementService by lazy {
+        ApiClient.create(context).create(ElementService::class.java)
     }
 }
 
