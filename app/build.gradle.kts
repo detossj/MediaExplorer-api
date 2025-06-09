26dcf42ea29568f3ea434c930c307f13bfcb7c39
detossj <jorgerubilarssj@gmail.com>
2025-06-08 20:00:56 -0400

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,13 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    //Api Rest
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     // Jetpack Compose Integration
     implementation("androidx.navigation:navigation-compose:$navVersion")
 
