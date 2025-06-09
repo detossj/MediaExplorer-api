31aa13258e05cbc3e0ff052a45098c0b8665bcb7
detossj <jorgerubilarssj@gmail.com>
2025-06-09 17:28:10 -0400

    functions added

--- app/src/main/java/com/deto/mediaexplorer/data/remote/services/CategoryService.kt
+++ app/src/main/java/com/deto/mediaexplorer/data/remote/services/CategoryService.kt
@@ -1,4 +1,15 @@
 package com.deto.mediaexplorer.data.remote.services
 
+import com.deto.mediaexplorer.data.Category
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface CategoryService {
+
+    @GET("api/categories")
+    suspend fun getCategories(): List<Category>
+
+    @POST("api/categories")
+    suspend fun addCategory(@Body category: Category): Category
 }
