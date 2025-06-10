360478f77809406902b1e420157924848faea6d6
detossj <jorgerubilarssj@gmail.com>
2025-06-10 18:20:59 -0400

    LaunchedEffect and uiState added

--- app/src/main/java/com/deto/mediaexplorer/ui/elements/ElementScreen.kt
+++ app/src/main/java/com/deto/mediaexplorer/ui/elements/ElementScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,8 +54,11 @@ import com.example.compose.secondaryContainerLight
 @Composable
 fun ElementScreen(navController: NavController, id: Int, viewModel: ElementViewModel = viewModel(factory = AppViewModelProvider.Factory)){
 
+    LaunchedEffect(id) {
+        viewModel.getElementsById(id)
+    }
 
-    val element by viewModel.getElement(id).collectAsState(initial = null)
+    val uiState = viewModel.elementUiState
     val scrollState = rememberScrollState()
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -73,41 +77,53 @@ fun ElementScreen(navController: NavController, id: Int, viewModel: ElementViewM
                     navigationIconContentColor = secondaryContainerLight
                 ),
                 title = {
-                    Column (
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 20.dp)
-                    ) {
-
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.End,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        ) {
-                            Text(
-                                text = element?.title ?: "",
-                                color = Color.White,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 32.sp,
-                                textAlign = TextAlign.Start,
-                                modifier = Modifier.weight(1f)
-                            )
-
-                            Text(
-                                text = element?.classification.toString(),
-                                fontSize = 18.sp
-                            )
-                            Icon(
-                                painter = painterResource(R.drawable.star_24px),
-                                contentDescription = "classification",
-                                modifier = Modifier
-                                    .padding(start = 4.dp)
-                                    .size(18.dp)
-                            )
+                    when (uiState) {
+                        is ElementUiState.Loading -> {
+                            Text(text = "Loading element...")
                         }
 
+                        is ElementUiState.Error -> {
+                            Text(text = "Error: ${uiState.message}")
+                        }
 
+                        is ElementUiState.Success -> {
+                            val element = uiState.element
+
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 20.dp)
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.End,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = element.title ?: "",
+                                        color = Color.White,
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 32.sp,
+                                        textAlign = TextAlign.Start,
+                                        modifier = Modifier.weight(1f)
+                                    )
+
+                                    Text(
+                                        text = element.classification.toString(),
+                                        fontSize = 18.sp
+                                    )
+                                    Icon(
+                                        painter = painterResource(R.drawable.star_24px),
+                                        contentDescription = "classification",
+                                        modifier = Modifier
+                                            .padding(start = 4.dp)
+                                            .size(18.dp)
+                                    )
+                                }
+                            }
+                        }
+
+                        else -> {}
                     }
                 }
 
@@ -149,53 +165,69 @@ fun ElementScreen(navController: NavController, id: Int, viewModel: ElementViewM
 
     ) { innerPadding ->
 
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ){
+        when (uiState) {
+            is ElementUiState.Loading -> {
+                Text(text = "Loading element...")
+            }
 
-            Column(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 20.dp).verticalScroll(scrollState)
-            ) {
+            is ElementUiState.Error -> {
+                Text(text = "Error: ${uiState.message}")
+            }
+
+            is ElementUiState.Success -> {
+                val element = uiState.element
 
                 Column(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier.padding(innerPadding)
                 ) {
-                    if(element?.imagen == null){
-                        Image(
-                            painter = painterResource(R.drawable.no_photography_24px),
-                            contentDescription = "Image Element",
-                            modifier = Modifier
-                                .size(300.dp)
-                                .align(Alignment.CenterHorizontally)
 
-                        )
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(horizontal = 20.dp, vertical = 20.dp)
+                            .verticalScroll(scrollState)
+                    ) {
 
-                    } else {
-                        Image(
-                            painter = painterResource(element?.imagen!!),
-                            contentDescription = "Image Element",
+                        Column(
                             modifier = Modifier
-                                .size(300.dp)
-                                .align(Alignment.CenterHorizontally)
-                        )
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            if (element?.imagen == null) {
+                                Image(
+                                    painter = painterResource(R.drawable.no_photography_24px),
+                                    contentDescription = "Image Element",
+                                    modifier = Modifier
+                                        .size(300.dp)
+                                        .align(Alignment.CenterHorizontally)
+
+                                )
+
+                            } else {
+                                Image(
+                                    painter = painterResource(element?.imagen!!),
+                                    contentDescription = "Image Element",
+                                    modifier = Modifier
+                                        .size(300.dp)
+                                        .align(Alignment.CenterHorizontally)
+                                )
+                            }
+
+                            Text(
+                                text = element?.description ?: "",
+                                fontSize = 14.sp,
+                                color = Color.White,
+                                modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)
+                            )
+
+                        }
+
                     }
-
-                    Text(
-                        text = element?.description ?: "",
-                        fontSize = 14.sp,
-                        color = Color.White,
-                        modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)
-                    )
 
                 }
 
             }
-
-
+            else -> {}
         }
-
     }
 }
