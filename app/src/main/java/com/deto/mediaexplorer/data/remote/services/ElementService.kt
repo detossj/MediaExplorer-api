6dd1b0ff50958f36a244f3856c29bfc16fc6b170
detossj <jorgerubilarssj@gmail.com>
2025-06-10 15:05:20 -0400

    getElements changed to getElementsByCategory

--- app/src/main/java/com/deto/mediaexplorer/data/remote/services/ElementService.kt
+++ app/src/main/java/com/deto/mediaexplorer/data/remote/services/ElementService.kt
@@ -5,11 +5,13 @@ import com.deto.mediaexplorer.data.Element
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ElementService {
 
     @GET("api/elements")
-    suspend fun getElements(): List<Element>
+    suspend fun getElementsByCategory(@Query("category_id") categoryId: Int): List<Element>
+
 
     @POST("api/elements")
     suspend fun addElement(@Body category: Element): Element
