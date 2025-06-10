62e4259674463fbd8b6346b8c70a3f0e7204a4a8
detossj <jorgerubilarssj@gmail.com>
2025-06-09 21:17:47 -0400

    class AuthViewModel added

--- app/src/main/java/com/deto/mediaexplorer/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/deto/mediaexplorer/ui/auth/AuthViewModel.kt
@@ -1,4 +1,76 @@
 package com.deto.mediaexplorer.ui.auth
 
-class AuthViewModel {
+import android.content.Context
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deto.mediaexplorer.data.remote.TokenManager
+import com.deto.mediaexplorer.data.remote.services.AuthService
+import com.deto.mediaexplorer.model.LoginRequest
+import kotlinx.coroutines.launch
+
+sealed class AuthUiState {
+    object Idle : AuthUiState()
+    object Loading : AuthUiState()
+    data class loggedIn(var logged: Boolean) : AuthUiState()
+    data class Success(val token: String) : AuthUiState()
+    data class Error(val message: String) : AuthUiState()
+
+}
+
+class AuthViewModel(
+    private val authService: AuthService,
+    private val context: Context
+) : ViewModel() {
+
+    var authState: AuthUiState by mutableStateOf(AuthUiState.Idle)
+        private set
+
+    init {
+        isLoggedIn()
+    }
+    fun login(email: String, password: String) {
+
+        viewModelScope.launch {
+
+            authState = AuthUiState.Loading
+
+            try {
+
+                val response = authService.login(LoginRequest(email, password))
+                TokenManager.saveToken(context, response.token)
+                authState = AuthUiState.Success(response.token)
+
+            } catch (e: Exception) {
+
+                authState = AuthUiState.Error(e.message ?: "Login failed")
+
+            }
+        }
+    }
+
+    fun logout() {
+        viewModelScope.launch {
+
+            try {
+                authService.logout()
+                TokenManager.clearToken(context)
+                authState = AuthUiState.Idle
+            } catch (_: Exception) {}
+        }
+    }
+
+    private fun isLoggedIn() {
+    viewModelScope.launch {
+            try {
+                val response = authService.getUser()
+                authState = AuthUiState.loggedIn(true)
+            } catch (e: Exception) {
+                authState = AuthUiState.loggedIn(false)
+            }
+        }
+    }
 }
