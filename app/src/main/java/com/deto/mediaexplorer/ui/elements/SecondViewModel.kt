9249d261b97ee44af5d1c26558cb6323147705a3
detossj <jorgerubilarssj@gmail.com>
2025-06-10 00:25:26 -0400

    ElementsUiState and functions added

--- app/src/main/java/com/deto/mediaexplorer/ui/elements/SecondViewModel.kt
+++ app/src/main/java/com/deto/mediaexplorer/ui/elements/SecondViewModel.kt
@@ -1,12 +1,25 @@
 package com.deto.mediaexplorer.ui.elements
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.deto.mediaexplorer.data.Element
 import com.deto.mediaexplorer.data.ElementRepository
+import com.deto.mediaexplorer.data.remote.services.ElementService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
+sealed class ElementsUiState {
+    data class Success(val elements: List<Element>) : ElementsUiState()
+    data class Error(val message: String) : ElementsUiState()
+    object Loading : ElementsUiState()
+    object Idle : ElementsUiState()
+}
+
+
+/*
 class SecondViewModel (private val elementRepository: ElementRepository) : ViewModel() {
 
     fun getElementsForCategory(categoryId: Int): Flow<List<Element>> {
@@ -21,4 +34,45 @@ class SecondViewModel (private val elementRepository: ElementRepository) : ViewM
 
 
 
+}*/
+
+class SecondViewModel(
+    private val elementService: ElementService
+) : ViewModel(){
+    var elementsUiState: ElementsUiState by mutableStateOf(ElementsUiState.Loading)
+        private set
+
+    fun resetUiState() {
+        elementsUiState = ElementsUiState.Idle
+    }
+
+    private fun getElements(){
+        viewModelScope.launch {
+            elementsUiState = ElementsUiState.Loading
+            elementsUiState = try {
+                val listElements = elementService.getElements()
+                ElementsUiState.Success(listElements)
+            } catch (e: Exception) {
+                ElementsUiState.Error(e.message ?: "Error desconocido")
+            }
+        }
+    }
+
+    fun refreshElements() {
+        getElements()
+    }
+
+    fun addElement(name: String, description: String, classification: Int) {
+        viewModelScope.launch {
+            elementsUiState = ElementsUiState.Loading
+            try {
+                elementService.addElement(Element(0,name,description,classification,null,0))
+                val updatedList = elementService.getElements()
+                elementsUiState = ElementsUiState.Success(updatedList)
+            } catch (e: Exception) {
+                elementsUiState = ElementsUiState.Error(e.message ?: "error")
+            }
+        }
+    }
 }
+
