c9f58749c8169c33b873afcafef0f0ca2b690c09
detossj <jorgerubilarssj@gmail.com>
2025-06-10 18:22:45 -0400

    ElementUiState and getElementById added

--- app/src/main/java/com/deto/mediaexplorer/ui/elements/ElementViewModel.kt
+++ app/src/main/java/com/deto/mediaexplorer/ui/elements/ElementViewModel.kt
@@ -1,13 +1,37 @@
 package com.deto.mediaexplorer.ui.elements
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.deto.mediaexplorer.data.Element
-import com.deto.mediaexplorer.data.ElementRepository
-import kotlinx.coroutines.flow.Flow
+import com.deto.mediaexplorer.data.remote.services.ElementService
+import kotlinx.coroutines.launch
 
-class ElementViewModel(private val elementRepository: ElementRepository) : ViewModel() {
+sealed class ElementUiState {
+    data class Success(val element: Element) : ElementUiState()
+    data class Error(val message: String) : ElementUiState()
+    object Loading : ElementUiState()
+    object Idle : ElementUiState()
+}
 
-    fun getElement(id : Int): Flow<Element> {
-        return elementRepository.getElementStream(id)
+class ElementViewModel(
+    private val elementService: ElementService
+) : ViewModel() {
+
+    var elementUiState: ElementUiState by mutableStateOf(ElementUiState.Loading)
+        private set
+
+    internal fun getElementsById(id: Int){
+        viewModelScope.launch {
+            elementUiState = ElementUiState.Loading
+            elementUiState = try {
+                val filteredElement = elementService.getElementById(id)
+                ElementUiState.Success(filteredElement)
+            } catch (e: Exception) {
+                ElementUiState.Error(e.message ?: "error")
+            }
+        }
     }
 }
