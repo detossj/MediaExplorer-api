682153e62b4552bb827473910cdfcc4a599d2b0e
detossj <jorgerubilarssj@gmail.com>
2025-06-10 18:22:05 -0400

    getElementById added

--- app/src/main/java/com/deto/mediaexplorer/data/remote/services/ElementService.kt
+++ app/src/main/java/com/deto/mediaexplorer/data/remote/services/ElementService.kt
@@ -5,6 +5,7 @@ import com.deto.mediaexplorer.data.Element
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ElementService {
@@ -12,6 +13,8 @@ interface ElementService {
     @GET("api/elements")
     suspend fun getElementsByCategory(@Query("category_id") categoryId: Int): List<Element>
 
+    @GET("api/elements/{id}")
+    suspend fun getElementById(@Path("id") id: Int): Element
 
     @POST("api/elements")
     suspend fun addElement(@Body category: Element): Element
