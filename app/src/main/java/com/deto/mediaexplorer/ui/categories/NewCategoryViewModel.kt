dfffe5874e84dfa625f6afa2e644fb859982775c
detossj <jorgerubilarssj@gmail.com>
2025-06-11 18:32:31 -0400

    names changed

--- app/src/main/java/com/deto/mediaexplorer/ui/categories/NewCategoryViewModel.kt
+++ app/src/main/java/com/deto/mediaexplorer/ui/categories/NewCategoryViewModel.kt
@@ -9,31 +9,31 @@ import com.deto.mediaexplorer.data.Category
 import com.deto.mediaexplorer.data.remote.services.CategoryService
 import kotlinx.coroutines.launch
 
-sealed class CategoryUiState {
-    data class Success(val categories: List<Category>) : CategoryUiState()
-    data class Error(val message: String) : CategoryUiState()
-    object Loading : CategoryUiState()
-    object Idle : CategoryUiState()
+sealed class NewCategoryUiState {
+    data class Success(val categories: List<Category>) : NewCategoryUiState()
+    data class Error(val message: String) : NewCategoryUiState()
+    object Loading : NewCategoryUiState()
+    object Idle : NewCategoryUiState()
 }
 class NewCategoryViewModel (private val categoryService: CategoryService) : ViewModel() {
 
-    var categoryUiState: CategoryUiState by mutableStateOf(CategoryUiState.Loading)
+    var newCategoryUiState: NewCategoryUiState by mutableStateOf(NewCategoryUiState.Loading)
         private set
 
     fun addCategory(name: String) {
         viewModelScope.launch {
-            categoryUiState = CategoryUiState.Loading
+            newCategoryUiState = NewCategoryUiState.Loading
             try {
                 categoryService.addCategory(Category(0,name,null))
                 val updatedList = categoryService.getCategories()
-                categoryUiState = CategoryUiState.Success(updatedList)
+                newCategoryUiState = NewCategoryUiState.Success(updatedList)
             } catch (e: Exception) {
-                categoryUiState = CategoryUiState.Error(e.message ?: "error")
+                newCategoryUiState = NewCategoryUiState.Error(e.message ?: "error")
             }
         }
     }
     fun resetUiState() {
-        categoryUiState = CategoryUiState.Idle
+        newCategoryUiState = NewCategoryUiState.Idle
     }
 
 }
