b1d254c756d22b672e701691e1ebca193cd4c7b8
detossj <jorgerubilarssj@gmail.com>
2025-06-14 14:25:40 -0400

    button deleted nad texts added

--- app/src/main/java/com/deto/mediaexplorer/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/deto/mediaexplorer/ui/auth/LoginScreen.kt
@@ -2,7 +2,6 @@ package com.deto.mediaexplorer.ui.auth
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -13,7 +12,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,9 +20,11 @@ import androidx.navigation.NavHostController
 import com.deto.mediaexplorer.HomePage
 import com.deto.mediaexplorer.Login
 import com.deto.mediaexplorer.R
+import com.deto.mediaexplorer.Register
 import com.deto.mediaexplorer.ui.AppViewModelProvider
 import com.deto.mediaexplorer.ui.components.CustomOutlinedTextField
 import com.example.compose.onPrimaryContainerLight
+import com.example.compose.primaryLight
 
 @Composable
 fun LoginScreen(
@@ -47,129 +47,120 @@ fun LoginScreen(
         }
     }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 32.dp)
+            .padding(top = 48.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_round),
+            contentDescription = "icon",
             modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 48.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_round),
-                contentDescription = "icon",
-                modifier = Modifier
-                    .size(100.dp)
-                    .padding(bottom = 8.dp),
-                contentScale = ContentScale.Fit
-            )
+                .size(100.dp)
+                .padding(bottom = 8.dp),
+            contentScale = ContentScale.Fit
+        )
 
-            Text(
-                text = stringResource(R.string.name_app),
-                fontSize = 18.sp,
-                textAlign = TextAlign.Center
-            )
+        Text(
+            text = stringResource(R.string.name_app),
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center
+        )
+
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+        Text(
+            text = stringResource(R.string.login),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+
+        CustomOutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            icon = R.drawable.mail_24px,
+            label = R.string.login_email_label,
+            placeholder = R.string.login_email_placeholder,
+            supportingText = R.string.login_email_supportingText,
+            isError = error,
+            iconTint = onPrimaryContainerLight,
+            textStyle = TextStyle(Color.Black)
+        )
+
+        CustomOutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            icon = R.drawable.password_24px,
+            label = R.string.login_password_label,
+            placeholder = R.string.login_password_placeholder,
+            supportingText = R.string.login_password_supportingText,
+            isError = error2,
+            iconTint = onPrimaryContainerLight,
+            textStyle = TextStyle(Color.Black)
+        )
+
+        Button(
+            onClick = {
+                error = email.isBlank()
+                error2 = password.isBlank()
+
+                if (!error && !error2) {
+                    viewModel.login(email, password)
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp)
+        ) {
+            Text(stringResource(R.string.sign_in))
         }
 
-        Column(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = stringResource(R.string.login),
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
-
-            CustomOutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                icon = R.drawable.mail_24px,
-                label = R.string.login_email_label,
-                placeholder = R.string.login_email_placeholder,
-                supportingText = R.string.login_email_supportingText,
-                isError = error,
-                iconTint = onPrimaryContainerLight,
-                textStyle = TextStyle(Color.Black)
-            )
-
-            CustomOutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                icon = R.drawable.password_24px,
-                label = R.string.login_password_label,
-                placeholder = R.string.login_password_placeholder,
-                supportingText = R.string.login_password_supportingText,
-                isError = error2,
-                iconTint = onPrimaryContainerLight,
-                textStyle = TextStyle(Color.Black)
-            )
-
-            Button(
-                onClick = {
-                    error = email.isBlank()
-                    error2 = password.isBlank()
-
-                    if (!error && !error2) {
-                        viewModel.login(email, password)
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp)
-            ) {
-                Text(stringResource(R.string.sign_in))
-            }
-
-            Spacer(modifier = Modifier.padding(10.dp))
-
-            Button(
-                onClick = { },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp)
-            ) {
-                Text(stringResource(R.string.sign_up))
+            Text(stringResource(R.string.login_message), fontSize = 16.sp)
+            TextButton(onClick = { navController.navigate(Register) }) {
+                Text(stringResource(R.string.login_message_button), color = primaryLight, fontSize = 16.sp)
             }
         }
 
-        Column(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            when (authState) {
-                is AuthUiState.Loading -> {
-                    CircularProgressIndicator()
-                }
+        Spacer(modifier = Modifier.height(20.dp))
 
-                is AuthUiState.Success -> {
-                    Text(
-                        text = stringResource(R.string.auth_successful),
-                        color = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier.padding(top = 8.dp)
-                    )
-                }
-
-                is AuthUiState.Error -> {
-                    Text(
-                        text = authState.message,
-                        color = MaterialTheme.colorScheme.error,
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.padding(top = 8.dp)
-                    )
-                }
-
-                else -> {}
+        when (authState) {
+            is AuthUiState.Loading -> {
+                CircularProgressIndicator()
             }
+
+            is AuthUiState.Success -> {
+                Text(
+                    text = stringResource(R.string.auth_successful),
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+
+            is AuthUiState.Error -> {
+                Text(
+                    text = authState.message,
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+
+            else -> {}
+
         }
+
     }
 
 }
+
